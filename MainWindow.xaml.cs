fb3df8ca61f8f85a7697a751d2c7a0f9b3bc9ffa
HUANG-TZU-HUAN <gino6487@gmail.com>
2023-05-08 00:53:15 +0800

    進度追蹤
    1.完成介面設計
    2.完成事件綁定與涵式

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,103 @@ namespace _111153008_Homework_4
         {
             InitializeComponent();
         }
+
+        void zero(string _one)
+        {
+            if(txtNumber.Text=="0")
+            {
+                txtNumber.Text = "";
+            }
+            txtNumber.Text+= _one;
+        }
+
+        private void btnOne_Click(object sender, RoutedEventArgs e)
+        {
+            zero("1");
+        }
+
+        private void btnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            zero("2");
+        }
+
+        private void btnThree_Click(object sender, RoutedEventArgs e)
+        {
+            zero("3");
+        }
+
+        private void btnFour_Click(object sender, RoutedEventArgs e)
+        {
+            zero("4");
+        }
+
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+            zero("5");
+        }
+
+        private void btnSix_Click(object sender, RoutedEventArgs e)
+        {
+            zero("6");
+        }
+
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
+        {
+            zero("7");
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+            zero("8");
+        }
+
+        private void btnNine_Click(object sender, RoutedEventArgs e)
+        {
+            zero("9");
+        }
+
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            zero("0");
+        }
+
+        void AMPD(string amp) 
+        { 
+            double first;
+            first = Convert.ToDouble(txtNumber.Text);
+            txtNumber.Text = "0";
+        }
+
+
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
